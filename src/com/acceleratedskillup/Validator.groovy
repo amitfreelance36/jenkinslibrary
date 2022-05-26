d400cf8aafd51cd963fe9a2d048a61ac1a8d34aa
AmitKumar <amitkr270390@gmail.com>
2022-05-26 17:35:21 +0530

    Adding this function to test the static function call from the pipeline script

---
+++ src/com/acceleratedskillup/Validator.groovy
@@ -0,0 +1,10 @@
+package com.acceleratedskillup
+
+class Valid {
+     static def validatorfunc(script)
+     {
+         script.echo "The static function is called from the script block"
+     }
+
+}
+
